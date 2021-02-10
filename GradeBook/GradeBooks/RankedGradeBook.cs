dbc06160a7a7d428832558ae8116d04ae5911082
Paul <shafferpm@gmail.com>
2021-02-09 20:56:46 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,6 +19,14 @@ namespace GradeBook.GradeBooks
             else
                 base.CalculateStatistics();
         }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStudentStatistics(name);
+        }
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
